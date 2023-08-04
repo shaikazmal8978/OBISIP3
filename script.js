c3c3a30da51407cf85fcc9fd31315d685e75734c
shaikazmal8978 <140682728+shaikazmal8978@users.noreply.github.com>
2023-08-04 19:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleContainer(containerId) {
+    var container = document.getElementById(containerId);
+    container.style.display = container.style.display === 'none' ? 'block' : 'none';
+  }
+  
